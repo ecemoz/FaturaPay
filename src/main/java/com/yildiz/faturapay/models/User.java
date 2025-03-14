867c09cf30258c8114aa28e3da7cc574d7c8dfd2
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-03-15 00:31:03 +0300

    Security Config is created.

--- src/main/java/com/yildiz/faturapay/models/User.java
+++ src/main/java/com/yildiz/faturapay/models/User.java
@@ -4,13 +4,13 @@ import com.yildiz.faturapay.utils.Role;
 import jakarta.persistence.*;
 import lombok.*;
 
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Entity
-    @Table(name = "users")
-    public class User {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
